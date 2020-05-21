bd7dc0ccdb0d86a56d8613cee6a6d6224f7774cf
Nikita Durasov <yassnda@gmail.com>
2020-05-21 14:13:02 +0300

    adding auto-complete settings as separate file

---
+++ .emacs.d/settings/auto-complete-settings.el
@@ -0,0 +1,18 @@
+;; Auto-complete
+(setq ac-directory (make-elget-path "auto-complete"))
+(add-to-list 'load-path ac-directory)
+(require 'auto-complete) 
+
+(require 'auto-complete-config) 
+(ac-config-default)
+;(global-auto-complete-mode t)
+
+;; dirty fix for having AC everywhere
+(define-globalized-minor-mode real-global-auto-complete-mode
+  auto-complete-mode (lambda ()
+		       (if (not (minibufferp (current-buffer)))
+			   (auto-complete-mode 1))
+		       ))
+(real-global-auto-complete-mode t)
+
+(provide 'auto-complete-settings)
